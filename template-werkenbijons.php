5acc3ab92459716724d90eab713c96490449f907
bakkouy2005 <154227141+bakkouy2005@users.noreply.github.com>
2025-09-02 14:59:35 +0200

    Create template-werkenbijons.php

---
+++ template-werkenbijons.php
@@ -0,0 +1,19 @@
+<?php
+/* Template Name: Werken bij ons */
+get_header();
+
+
+?>
+
+<div class="">
+    <?php get_template_part('template-parts/test'); ?>
+   
+   
+</div>
+
+<?php
+
+
+get_footer();
+
+?>
