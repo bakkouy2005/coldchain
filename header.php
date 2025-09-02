37b609393cf273f8bd165dfe6eac2dd63ca36e6b
joullutfi <joullutfi76@outlook.com>
2025-09-02 13:46:26 +0200

    Update header.php

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
             <ul>
                 <li><a href="#">Home</a></li>
                 <li><a href="#">About</a></li>
-                <li><a href="#">Contact</a></li>
+                <li><a href="#">Contacst</a></li>
             </ul>
         </nav>
     </header>
