2e7ae14595e156ccc92a3b329b8bbd43c26b67f5
bakkouy2005 <154227141+bakkouy2005@users.noreply.github.com>
2025-09-17 15:11:49 +0200

    f

---
+++ template-parts/reviews.php
@@ -0,0 +1,6 @@
+<div class="reviews-section">
+  <div class="container mx-auto px-4">
+    <h2 class="text-4xl font-bold text-center mb-12 text-gray-700">Reviews</h2>
+    <?php echo do_shortcode('[repocean_reviews layout="slider_v4"]'); ?>
+  </div>
+</div>
