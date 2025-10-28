e88ed5046b670da2568cf705a11bdd0a2766bc98
joullutfi <joullutfi76@outlook.com>
2025-10-28 10:36:40 +0100

    geen revieuws

--- template-homepage.php
+++ template-homepage.php
@@ -12,7 +12,7 @@ get_header();
     <?php get_template_part('template-parts/service'); ?>
     <?php get_template_part('template-parts/overons_info'); ?>
     <?php get_template_part('template-parts/overons_fototext'); ?>
-    <?php get_template_part('template-parts/reviews'); ?>
+    
 </div>
 
 <?php
