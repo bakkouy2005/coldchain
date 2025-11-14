42a13b12c68f6c6cf4d3b9261cea22b92e3e4623
bakkouy2005 <154227141+bakkouy2005@users.noreply.github.com>
2025-11-14 10:44:16 +0100

    Update template-cookiebeleid.php

--- template-cookiebeleid.php
+++ template-cookiebeleid.php
@@ -1,15 +1,13 @@
 <?php
 /* Template Name: Cookiebeleid */
+
 get_header();
 
-
-?>
-
-<?php get_the_content(); ?>
-
-<?php
-
+if ( have_posts() ) :
+    while ( have_posts() ) :
+        the_post();
+        the_content(); // This will render the page content and process shortcodes like [cmplz-document]
+    endwhile;
+endif;
 
 get_footer();
-
-?>
